d239352fe159d53d3179764ce7c448b4f88ce79a
idlemike <idlemike@yandex.ru>
2020-08-24 14:23:56 +0300

    rate-limiter

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,9 +6,13 @@ const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 const sendEmail = require("./../utils/email");
 
+const { JWT_SECRET = 'dev-key' } = process.env;
+const { JWT_EXPIRES_IN = '90d' } = process.env;
+const { JWT_COOKIE_EXPIRES_IN = '90' } = process.env;
+
 const signToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+  return jwt.sign({ id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN
   });
 };
 
@@ -16,7 +20,7 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
   const cookieOptions = {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
